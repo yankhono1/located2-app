4f12b74b9c87201b907335cc66d79b221de93851
Yankho Mpokosa <113061371+yankhono1@users.noreply.github.com>
2022-11-03 13:10:02 +0000

    Update schema.js

--- schema.js
+++ schema.js
@@ -3,15 +3,13 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
+import user from './user';
+import pin from './pin';
+import comment from './comment';
+import postedBy from './postedBy';
+import save from './save';
 // We import object and document schemas
-import blockContent from './blockContent'
-import crewMember from './crewMember'
-import castMember from './castMember'
-import movie from './movie'
-import person from './person'
-import screening from './screening'
-import plotSummary from './plotSummary'
-import plotSummaries from './plotSummaries'
+
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -22,15 +20,6 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    movie,
-    person,
-    screening,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-    plotSummary,
-    plotSummaries,
-    castMember,
-    crewMember,
+    user, pin, comment, postedBy, save
   ]),
 })
